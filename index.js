909fa7375ab0cbcca0d0a55a41d7c15ec9126092
Phuc Nguyen <phucnguyen@okxe.vn>
2021-02-20 16:41:26 +0700

    hello

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function(req, red) {
     red.send('Hello You')
 })
 
+app.get('/about', function(req, red) {
+    red.send('Hello about')
+})
+
 app.listen(3000, function() {
     console.log('example app with jenkins')
 })
